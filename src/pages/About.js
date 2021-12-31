d08559fa41585d7c5bf6abff5be0bd553cf33ac6
mohammadsajadi <mohammadsajadi50@gmail.com>
2021-12-31 13:55:10 +0330

    part3

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageName= "About"  pageTitle="About me "  >
+<h1>this is about page</h1>
+<p1>  hi this is my about page and i write this using layout component </p1>
+    </Layout>
+  )
+}
+// Step 3: Export your component
+export default AboutPage
